73d8ad88929445422caee53b838ad05906764f3c
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-07-12 18:52:21 +0200

    Update create.php

--- create.php
+++ create.php
@@ -1,5 +1,5 @@
  <?php 
- session_start();
+    require "dbconnection.php";
   $errorMessages=array(); //associative array to carry the errors during check the validation 
   
 function CleanInputs($input)
@@ -15,42 +15,42 @@ function CleanInputs($input)
      $name =CleanInputs($_POST["name"]);  
      $content =CleanInputs($_POST["content"]);  
 
-     //check the file 
-     if(!empty($_FILES['uploadedFile']['name'])&& isset($_FILES['uploadedFile']['name'])){ //check if the file is uploded or not
-        $PathOfTemp = $_FILES['uploadedFile']['tmp_name']; //path of temp is the name of the file when is uploaded and is putted in the temp folder in the server 
-        $nameOfTheFile = $_FILES['uploadedFile']['name']; //the Original name of the file that is in the user's Device. Note: the name is sent and included the extension of the uploaded file
-        $sizeOfTheFile = $_FILES['uploadedFile']['size']; 
-        $type = $_FILES['uploadedFile']['type'];//the type of the uploaded file 
+     //check the Image 
+     if(!empty($_FILES['image']['name'])&& isset($_FILES['image']['name'])){ //check if the file is uploded or not
+        $PathOfTemp = $_FILES['image']['tmp_name']; //path of temp is the name of the file when is uploaded and is putted in the temp folder in the server 
+        $nameOfTheImage = $_FILES['image']['name']; //the Original name of the file that is in the user's Device. Note: the name is sent and included the extension of the uploaded file
+        $sizeOfTheImage = $_FILES['image']['size']; 
+        $type = $_FILES['image']['type'];//the type of the uploaded file 
         
-        $array= explode('.',$nameOfTheFile);//separate the name string to get the extension of the uploaded File 
+        $array= explode('.',$nameOfTheImage);//separate the name string to get the extension of the uploaded File 
         $FileExtension = strtolower($array[1]); // get the extension of the uploaded File :)
 
 
         // to prevent the error when two users or more uploaded file at the same time with the same name as example 
         //I need to create new name for every file will be uploaded and Must this Name is Unique 
-        $FinalNameOfTheFile = rand().time().'.'.$FileExtension;
+        $FinalNameOfTheImage = rand().time().'.'.$FileExtension;
         
         // Now ,I need to Limit the types of the uploaded Files 
-        $limitedExtensions = ['png','jpg','pdf']; //note : sometimes , some Files are uploaded with extensions Like As Example:JPG,PNG,PDF .and that will be prevent the files with capital letters to be uploaded. to solve with this problem I will make the extension with lower letters by Built-In function => strtolower() 
+        $limitedExtensions = ['png','jpg']; //note : sometimes , some Files are uploaded with extensions Like As Example:JPG,PNG,PDF .and that will be prevent the files with capital letters to be uploaded. to solve with this problem I will make the extension with lower letters by Built-In function => strtolower() 
         //to check if the uploaded file with the limited Extensions
         if(in_array($FileExtension,$limitedExtensions)){
             // the uploaded file extension is in limitedExtensions :) . Now ,I can uploaded the file and get the file from the temp folder in the server to myuploads folder that I previously created in the server 
             $myDistenationFolder ='./myuploads/';
-            $destinationFile = $myDistenationFolder.$FinalNameOfTheFile;
+            $destinationFile = $myDistenationFolder.$FinalNameOfTheImage;
             if(move_uploaded_file($PathOfTemp,$destinationFile)){
                 $fileIsUploadedToDesFolder ='File Uploaded';
                 
             }else{
-                 $errorMessages['file'] = 'error in Upload the file Please try again later !';
+                 $errorMessages['image'] = 'error in Upload the file Please try again later !';
             }
             
             
         }else{
-              $errorMessages['file'] = 'error Extension Is not allowed !';
+              $errorMessages['image'] = 'error Extension Is not allowed !';
         }
          
      }else{
-            $errorMessages['file'] = 'error your file is required please uploaded it!';
+            $errorMessages['image'] = 'error your file is required please uploaded it!';
      }
      
      // Name Validation ...
@@ -61,21 +61,7 @@ function CleanInputs($input)
         }else{
           $errorMessages['name'] = " your Name is Required!";
         }
-     //check the email
-       if(!empty($email)){
-        
-            if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
-                 $s_email = filter_var($email,FILTER_SANITIZE_EMAIL);
-                   if(!filter_var($s_email,FILTER_VALIDATE_EMAIL)){
-                        $errorMessages['email'] = 'error your Email is not valid! ';
-              
-                    }
-        
-             }
-        }else{
-                  $errorMessages['email'] = 'error Email Required!';
-            }
-    
+     
  
     
      //print the result 
@@ -83,7 +69,13 @@ function CleanInputs($input)
      if(count($errorMessages) == 0){
        //form is valid then print the data 
         // echo 'your name is : '.$name.'<br> your email is :'.$email.'<br> your password is:'.$password.'<br> your LinkedIn Account is:'.$AccountOfLinkedIn.'<br>' .$fileIsUploadedToDesFolder.'<br> and your file path is :'.$nameOfTheFile;
-      
+        $sql = "INSERT INTO posts(name,content,image) VALUES ('$name','$content','$nameOfTheImage');";
+        $op = mysqli_query($conn,$sql);
+        if($op){
+              echo'Data Inserted';
+        }else{
+            echo'ERROR In Insert the data ';
+        }
      }else{
 
      // print error messages 
@@ -127,7 +119,7 @@ function CleanInputs($input)
 
                  <div class="form-group">
                      <label for="exampleInputEmail1">Enter your Content</label>
-                     <input type="email" name="content" class="form-control" id="exampleInputEmail1"
+                     <input type="text" name="content" class="form-control" id="exampleInputEmail1"
                          placeholder="Enter your content">
                  </div>
 
