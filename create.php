41d9bfee60d16e15fd097e327f333c7b00a98048
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-07-12 18:27:24 +0200

    Update create.php

--- create.php
+++ create.php
@@ -13,9 +13,8 @@ function CleanInputs($input)
  if($_SERVER['REQUEST_METHOD']=="POST"){
      
      $name =CleanInputs($_POST["name"]);  
-     $email =CleanInputs($_POST["email"]);  
-     $password =CleanInputs($_POST["password"]);  
-     $AccountOfLinkedIn=$_POST["account"];
+     $content =CleanInputs($_POST["content"]);  
+
      //check the file 
      if(!empty($_FILES['uploadedFile']['name'])&& isset($_FILES['uploadedFile']['name'])){ //check if the file is uploded or not
         $PathOfTemp = $_FILES['uploadedFile']['tmp_name']; //path of temp is the name of the file when is uploaded and is putted in the temp folder in the server 
@@ -76,46 +75,15 @@ function CleanInputs($input)
         }else{
                   $errorMessages['email'] = 'error Email Required!';
             }
-    // Password Validation ... 
-        if(!empty($password)){
-            if(strlen($password) < 6){
-
-               $errorMessages['Password'] = "Password Length must be > 5 "; 
-            }
-
-        }else{
-
-          $errorMessages['Password'] = " your Password Required";
-
-        }
-    // account Validation
-    if(!empty( $AccountOfLinkedIn)){
-         $AccountOfLinkedIn=trim($AccountOfLinkedIn);
-         $AccountOfLinkedIn=htmlspecialchars($AccountOfLinkedIn);
-        if(!filter_var( $AccountOfLinkedIn,FILTER_VALIDATE_URL)){
-             $s_URL = filter_var($AccountOfLinkedIn,FILTER_SANITIZE_URL);
-              if(!filter_var($s_URL,FILTER_VALIDATE_URL)){
-                  $errorMessages['url_account'] = 'error your URl is not valid! ';
-              
-              }
-        
-        }
-    }else{
-      $errorMessages['url_account'] = 'error URL is Required!';
-}
     
+ 
     
      //print the result 
      
      if(count($errorMessages) == 0){
        //form is valid then print the data 
         // echo 'your name is : '.$name.'<br> your email is :'.$email.'<br> your password is:'.$password.'<br> your LinkedIn Account is:'.$AccountOfLinkedIn.'<br>' .$fileIsUploadedToDesFolder.'<br> and your file path is :'.$nameOfTheFile;
-        $_SESSION['name']=$name;
-        $_SESSION['email']=$email;
-        $_SESSION['password']=$password;
-        $_SESSION['account']=$AccountOfLinkedIn;
-        $_SESSION['file']=$fileIsUploadedToDesFolder;
-        $_SESSION['file']=$nameOfTheFile;
+      
      }else{
 
      // print error messages 
@@ -135,7 +103,7 @@ function CleanInputs($input)
  <html lang="en">
 
      <head>
-         <title>ValidationForm</title>
+         <title>register </title>
          <meta charset="utf-8">
          <meta name="viewport" content="width=device-width, initial-scale=1">
          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -146,39 +114,31 @@ function CleanInputs($input)
      <body>
 
          <div class="container">
-             <h2> Form</h2>
+             <h2> create post  </h2>
              <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>"
                  enctype="multipart/form-data">
 
                  <div class="form-group">
-                     <label for="exampleInputEmail1">Enter your Name</label>
+                     <label for="exampleInputEmail1">Enter The Post Name</label>
                      <input type="text" name="name" class="form-control" id="exampleInputName" aria-describedby=""
-                         placeholder="Enter Name">
+                         placeholder="Enter The post name ">
                  </div>
 
 
                  <div class="form-group">
-                     <label for="exampleInputEmail1">Enter your Email</label>
-                     <input type="email" name="email" class="form-control" id="exampleInputEmail1"
-                         aria-describedby="emailHelp" placeholder="Enter email">
+                     <label for="exampleInputEmail1">Enter your Content</label>
+                     <input type="email" name="content" class="form-control" id="exampleInputEmail1"
+                         placeholder="Enter your content">
                  </div>
 
+                 
+                 
                  <div class="form-group">
-                     <label for="exampleInputPassword1">Enter your Password</label>
-                     <input type="password" name="password" class="form-control" id="exampleInputPassword1"
-                         placeholder="Password">
-                 </div>
-                 <div class="form-group">
-                     <label for="exampleInputPassword1">Please enter your LinkedIn account </label>
-                     <input type="url" name=" account" class="form-control" id="exampleInputPassword1"
-                         placeholder="https://www.linkedIn.com/">
-                 </div>
-                 <div class="form-group">
-                     <label for="exampleInputEmail1">Enter your Cv</label>
-                     <input type="file" name="uploadedFile" id=" exampleInputName" aria-describedby="">
+                     <label for="exampleInputEmail1">Enter An Image of The Post</label>
+                     <input type="file" name="image" id=" exampleInputName" aria-describedby="">
                  </div>
 
-                 <button type="submit" class="btn btn-primary">Submit</button>
+                 <button type="submit" class="btn btn-primary">create Post</button>
              </form>
          </div>
 
